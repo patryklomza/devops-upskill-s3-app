231718b8d818bd3b0c8b7ea72bf564615f0845e2
Patryk Lomza <p.lomza@outlook.com>
2021-09-09 13:58:27 +0200

    Add initial project code

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
-__version__ = '0.1.0'
+from flask import Flask
+
+
+def create_app(config_name=None):
+    from .main.views import s3_form
+
+    app = Flask(__name__)
+    app.register_blueprint(s3_form)
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+    return app
